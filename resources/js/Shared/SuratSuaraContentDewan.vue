778fb6c72e4f6c6212fbe758ac64236228dae130
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-01-15 14:52:53 +0700

    add object contain to logo asset

--- resources/js/Shared/SuratSuaraContentDewan.vue
+++ resources/js/Shared/SuratSuaraContentDewan.vue
@@ -2,7 +2,7 @@
     <div class="bg-white overflow-hidden shadow-xl shadow-slate-600 mr-4 mb-10 border border-2 border-slate-950">
         <div>
             <div class="bg-red-600 flex items-center justify-between pt-5 px-5 min-h-20">
-                <img v-bind:src="'assets/img/KPU_Logo.svg.webp'" class="w-10 w-12 lg:w-20 lg:h-20" alt="">
+                <img v-bind:src="'assets/img/KPU_Logo.svg.webp'" class="object-contain w-10 w-12 lg:w-20 lg:h-20" alt="">
                 <div class="block text-center font-bold lg:text-xl">
                     <h3 class="text-white">
                         SURAT SUARA PEMILIHAN UMUM<br>
@@ -11,7 +11,7 @@
                         TAHUN 2024
                     </h3>
                 </div>
-                <img v-bind:src="'assets/img/Logo_Pemilu_Sarana_Integrasi_Bangsa_2024.svg'" class="w-10 w-12 lg:w-20 lg:h-28" alt="">
+                <img v-bind:src="'assets/img/Logo_Pemilu_Sarana_Integrasi_Bangsa_2024.svg'" class="object-contain w-10 w-12 lg:w-20 lg:h-28" alt="">
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35.28 2.17" preserveAspectRatio="none" class="fill-current text-red-600">
                 <path d="M0 1c3.17.8 7.29-.38 10.04-.55 2.75-.17 9.25 1.47 12.67 1.3 3.43-.17 4.65-.84 7.05-.87 2.4-.02 5.52.88 5.52.88V0H0z" fill="%23ff0000"/>
