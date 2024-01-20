1abf2a6e4ff3a39d39d402d0e348d8be296d3ed0
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-01-20 07:46:09 +0700

    add component back to top

---
+++ resources/js/Shared/BackToTop.vue
@@ -0,0 +1,15 @@
+<template>
+    <back-to-top class="hidden md:block bg-red-500 bg-opacity-95 hover:bg-opacity-80 font-semibold text-white rounded-lg p-2">
+        <button type="button" class="">Kembali Ke Atas</button>
+    </back-to-top>
+    <back-to-top class="md:hidden bg-red-500 bg-opacity-95 hover:bg-opacity-80 font-semibold text-white rounded-lg p-2">
+        <button type="button" class="text-xl">
+            <ChevronUpIcon class="h-8 w-8" aria-hidden="true"/>
+        </button>
+    </back-to-top>
+</template>
+<script setup>
+import BackToTop from 'vue-backtotop'
+import { ChevronUpIcon } from '@heroicons/vue/24/outline'
+
+</script>
