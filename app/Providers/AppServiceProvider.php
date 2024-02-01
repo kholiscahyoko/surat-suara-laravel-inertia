6d0b0ebfeadd99164a79035090ce6fad37c9f550
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-01 11:57:27 +0700

    Revert "add redirect https"
    This reverts commit c8c0f37991b19fb541df32664e3742f534eb1a7b.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\App;
-use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,10 +17,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-        if (App::environment(['staging', 'production'])) {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
