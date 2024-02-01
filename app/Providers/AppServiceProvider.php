c8c0f37991b19fb541df32664e3742f534eb1a7b
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-01 11:10:39 +0700

    add redirect https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +19,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        if (App::environment(['staging', 'production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
