3d3cb0485162a59b6a8469cc2bbfc1db69c0a527
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-04 13:17:11 +0700

    update min h block dewan

--- resources/js/Components/CalonDewanBlock.vue
+++ resources/js/Components/CalonDewanBlock.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :id="`partai${partai.no_urut}`" class="block-dewan w-64 md:w-52 lg:w-64 md:min-h-[440px] bg-white overflow-hidden shadow-xl shadow-slate-600 m-2 mb-10 border-2 border-slate-950  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer">
+    <div :id="`partai${partai.no_urut}`" class="block-dewan w-64 md:w-52 lg:w-64 sm:min-h-[440px] bg-white overflow-hidden shadow-xl shadow-slate-600 m-2 mb-10 border-2 border-slate-950  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer">
         <div class="flex items-center px-4 py-4 border-b border-black">
             <div class="font-bold w-10 h-5 text-3xl md:text-xl lg:text-3xl text-center">{{ partai.no_urut }}</div>
             <img v-bind:src="$setUrl(`/assets/img/partai/${partai.no_urut}.webp`)" class="w-16 h-16 md:w-12 md:h-16 lg:w-20 lg:h-20 mx-2 object-contain" :alt="partai.nama" loading="lazy">
