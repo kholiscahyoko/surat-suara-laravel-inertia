1fd1bea18fa96988ee5983b116950207ebe58e74
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-06 14:43:17 +0700

    update nav link size

--- resources/js/Shared/NavLink.vue
+++ resources/js/Shared/NavLink.vue
@@ -1,6 +1,6 @@
 <template>
     <Link
-    :class="[active ? 'bg-white text-red-700' : 'text-gray-100 hover:bg-gray-200 hover:text-red-600', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="active ? 'page' : undefined"
+    :class="[active ? 'bg-white text-red-700' : 'text-gray-100 hover:bg-gray-200 hover:text-red-600', 'rounded-md px-3 py-2 font-medium']" :aria-current="active ? 'page' : undefined"
     >
         <slot />
     </Link>
