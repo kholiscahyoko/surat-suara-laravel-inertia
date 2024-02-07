c75d3b19fe9bc59e49f7b9523776ea20367060d3
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-07 10:50:39 +0700

    update footer

--- resources/js/Shared/Footer.vue
+++ resources/js/Shared/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="bg-red-600 shadow mt-4 ">
         <div class="p-4 sm:items-center">
-            <div class="text-base text-white text-center">&copy; 2024 <a href="https://me.kholiscahyoko.my.id/" class="hover:underline">Shotaro Hidari</a>.
+            <div class="text-base text-white text-center">&copy; 2024 <a href="https://me.kholiscahyoko.my.id/" class="hover:underline">Shotaro Hidari /W</a>.
             </div>
         </div>
     </footer>
