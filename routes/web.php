51076a82e419404a5cb7974d8a8a59e2ad4bd3ca
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-10 00:08:51 +0700

    takeout dashboard

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,3 @@ Route::post('/logout', function(){
 });
 
 Route::get('/get_list_wilayah_by_dapil', [SuratSuaraController::class, 'get_list_wilayah_by_dapil']);
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-});
