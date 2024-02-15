345fe57b3ad7bb1e5c75f55da595788f0a005c7e
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-15 17:59:22 +0700

    add gtag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,15 @@
     @inertiaHead
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3686539766588397"
      crossorigin="anonymous"></script>
+     <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CH7DT4270T"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-CH7DT4270T');
+    </script>
   </head>
   <body class="min-h-full">
     @inertia
