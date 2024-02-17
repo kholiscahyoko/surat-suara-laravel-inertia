3d68149f7bc041ffcb164167a5af4ed1b95511ea
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-17 09:41:43 +0700

    update footer label

--- resources/js/Shared/Footer.vue
+++ resources/js/Shared/Footer.vue
@@ -6,7 +6,7 @@
                 <Link href="/kebijakan-privasi" class="px-4 text-nowrap hover:underline">Kebijakan Privasi</Link>
                 <Link href="/hubungi-kami" class="px-4 text-nowrap hover:underline">Hubungi Kami</Link>
             </div>
-            <div class="text-base text-white text-center">&copy; 2024 <a href="https://me.kholiscahyoko.my.id/" class="hover:underline text-purple-500">Shotaro Hidari <span class="text-white">/W</span></a>.
+            <div class="text-base text-white text-center">&copy; 2024 <a href="https://me.kholiscahyoko.my.id/" class="hover:underline text-purple-500">Kholis Cahyoko </a>.
             </div>
         </div>
     </footer>
