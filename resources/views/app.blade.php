82d4731c64ac4516fa6804884182411d825b35d7
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-20 05:51:19 +0700

    update language

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="h-full bg-gray-100" lang="en">
+<html class="h-full bg-gray-100" lang="id">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
