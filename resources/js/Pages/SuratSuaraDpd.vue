17890f080e8179a4b1df0758a64caaa2e2b1e42e
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-20 19:44:46 +0700

    add entry point realcount dpd

--- resources/js/Pages/SuratSuaraDpd.vue
+++ resources/js/Pages/SuratSuaraDpd.vue
@@ -1,7 +1,13 @@
 <template>
-    <h1 class="text-3xl font-bold tracking-tight text-gray-900 mb-4 drop-shadow-md shadow-black">
+    <div class="md:flex md:justify-between">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900 mb-4 drop-shadow-md shadow-black">
         Calon DPD RI
-    </h1>
+      </h1>
+      <div class="flex">
+        <a :href="$setUrl(`/hitung-suara/${dapil.jenis_dewan}/${$slugify(dapil.nama_dapil)}/${dapil.kode_dapil}`)" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-3 focus:outline-none">Hitung Suara</a>
+      </div>
+    </div>
+
     <SuratSuaraContentDpd :calons="calons" :dapil="dapil" :calon_id="calon_id"/>
 </template>
 <script setup>
