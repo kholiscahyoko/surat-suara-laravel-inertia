9aa5f9158b919736fd468c1438e9b7a69c52a25d
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-02-28 23:20:30 +0700

    update wording fitur baru

--- resources/js/Shared/FiturBaru.vue
+++ resources/js/Shared/FiturBaru.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="rounded-lg p-4 md:p-6 m-3 shadow-xl shadow-slate-600 bg-white">
-        <h3 class="text-center font-bold text-xl">Hitung Suara</h3>
+        <h3 class="text-center font-bold text-xl">Fitur Baru</h3>
         <div class="flex flex-wrap font-serif mt-2">
             <p>
-                Fitur real count KPU dikembangkan bertahap. Saat ini untuk Realcount Pilpres sudah bisa diakses lewat tombol <span class="font-semibold">Real Count Pilpres</span> di Halaman Utama dan di Surat Suara Pilpres. Sedangkan <span class="font-semibold">Real Count DPD</span> dan <span class="font-semibold">DPR</span> bisa diakses melalui pencarian dapil, halaman Surat Suara DPD/DPR, dan halaman profil calon anggota DPD/DPR.
+                <span class="font-semibold">Calon DPD Terpilih</span> diakses via pencarian dapil, halaman Detail Surat Suara, dan halaman realcount/hitung suara <span class="font-semibold">DPD</span>.
+            </p>
+            <p>
+                <span class="font-semibold">Real Count Pilpres </span> sudah diakses via tombol <span class="font-semibold">Real Count Pilpres</span> di Halaman Utama dan di Surat Suara Pilpres. Sedangkan <span class="font-semibold">Real Count DPD/DPR/DPRD</span> diakses melalui pencarian dapil, halaman Detail Surat Suara, dan halaman profil calon DPD/DPR/DPRD.
             </p>
         </div>
     </div>
