36ec8f941ed4794680e3d7fd97a2a06aaad6ee26
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-03-01 06:20:04 +0700

    add rel nofollow to external links

--- resources/js/Shared/KawalPemilu.vue
+++ resources/js/Shared/KawalPemilu.vue
@@ -3,20 +3,20 @@
         <h3 class="text-center font-bold text-xl">KawalPemilu.org</h3>
         <div class="flex flex-wrap font-serif">
             <p>
-                Ayo bagikan & ajak sebanyak-banyaknya orang di seluruh Indonesia, untuk ikut <a class="text-indigo-600 hover:text-indigo-900" href="https://kawalpemilu.org">#UnggahInputKirim</a> -> Unggah foto C.Hasil Plano, Input Perolehan Suara, Kirim ke <a class="text-indigo-600 hover:text-indigo-900" href="https://kawalpemilu.org">http://kawalpemilu.org</a>.
+                Ayo bagikan & ajak sebanyak-banyaknya orang di seluruh Indonesia, untuk ikut <a class="text-indigo-600 hover:text-indigo-900" rel="nofollow" target="_blank" href="https://kawalpemilu.org">#UnggahInputKirim</a> -> Unggah foto C.Hasil Plano, Input Perolehan Suara, Kirim ke <a class="text-indigo-600 hover:text-indigo-900" rel="nofollow" target="_blank" href="https://kawalpemilu.org">http://kawalpemilu.org</a>.
             </p>
             <p>
-                Semakin banyak yang <a class="text-indigo-600 hover:text-indigo-900" href="https://kawalpemilu.org">#UnggahInputKirim</a> - maka penghitungan swadaya ini akan semakin transparan dan akurat <a class="text-indigo-600 hover:text-indigo-900" href="https://kawalpemilu.org">#kawalpemilu2024</a>
+                Semakin banyak yang <a class="text-indigo-600 hover:text-indigo-900" rel="nofollow" target="_blank" href="https://kawalpemilu.org">#UnggahInputKirim</a> - maka penghitungan swadaya ini akan semakin transparan dan akurat <a class="text-indigo-600 hover:text-indigo-900" rel="nofollow" target="_blank" href="https://kawalpemilu.org">#kawalpemilu2024</a>
             </p>
         </div>
         <div class="mx-auto font-serif mt-5">
             <h4 class="text-center">Media Sosial</h4>
             <div class="flex flex-wrap justify-center">
-                <a href="https://twitter.com/kawalpemilu_org" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Twitter</a>
-                <a href="https://www.instagram.com/kawalpemilu_org" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Instagram</a>
-                <a href="https://www.facebook.com/KawalPemilu" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Facebook</a>
-                <a href="https://www.threads.net/@kawalpemilu_org" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Threads</a>
-                <a href="https://www.tiktok.com/@kawalpemilu_org" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Tiktok</a>
+                <a href="https://twitter.com/kawalpemilu_org" rel="nofollow" target="_blank" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Twitter</a>
+                <a href="https://www.instagram.com/kawalpemilu_org" rel="nofollow" target="_blank" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Instagram</a>
+                <a href="https://www.facebook.com/KawalPemilu" rel="nofollow" target="_blank" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Facebook</a>
+                <a href="https://www.threads.net/@kawalpemilu_org" rel="nofollow" target="_blank" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Threads</a>
+                <a href="https://www.tiktok.com/@kawalpemilu_org" rel="nofollow" target="_blank" class="px-4 text-nowrap text-indigo-600 hover:text-indigo-900 underline underline-offset-4 decoration-2">Tiktok</a>
             </div>
         </div>
     </div>
--- resources/js/Shared/ProfilDpd.vue
+++ resources/js/Shared/ProfilDpd.vue
@@ -48,7 +48,7 @@
                 <div>
                     <p class="font-semibold">Sumber Data</p>
                     <p>
-                        <a href="https://infopemilu.kpu.go.id/Pemilu/Peserta_pemilu" class="text-indigo-600 hover:text-indigo-900">DCT Info Pemilu KPU</a>, terakhir akses tanggal 18 Januari 2024
+                        <a href="https://infopemilu.kpu.go.id/Pemilu/Peserta_pemilu" rel="nofollow" target="_blank" class="text-indigo-600 hover:text-indigo-900">DCT Info Pemilu KPU</a>, terakhir akses tanggal 18 Januari 2024
                     </p>
                 </div>
             </div>
