2c3e7657bfdf5eaab9f4d54389cbf53006650cd9
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-03-01 07:53:16 +0700

    leaflet set scroll wheel zoom false

--- resources/js/Components/Leaflet.vue
+++ resources/js/Components/Leaflet.vue
@@ -2,7 +2,7 @@
   <div class="bg-white mb-10 p-2 shadow-lg shadow-slate-800 rounded-lg">
     <h2 class="lg:text-2xl text-xl text-center font-bold mb-2">Daftar Calon Tetap di setiap provinsi</h2>
     <div class="w-full h-[500px] ">
-      <l-map ref="map" v-model:zoom="zoom" :min-zoom="(zoom-1)" :max-zoom="(zoom+2)" :center="center" :use-global-leaflet="false">
+      <l-map ref="map" v-model:zoom="zoom" :min-zoom="(zoom-1)" :max-zoom="(zoom+2)" :center="center" :use-global-leaflet="false" :options="{scrollWheelZoom: false}">
         <l-tile-layer
           url="https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
           layer-type="base"
@@ -21,8 +21,7 @@
   
 <script setup>
 import "leaflet/dist/leaflet.css";
-import { ref, onMounted, onUnmounted } from 'vue'
-import { LMap, LTileLayer, LGeoJson, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
 
 let props = defineProps({
   data_provinsi: Array,
