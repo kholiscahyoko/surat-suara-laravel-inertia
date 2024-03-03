7cd94e2d64e52105d7a7e957e54111eafbd371dc
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-03-03 16:01:49 +0700

    git status

--- resources/js/Shared/FiturBaru.vue
+++ resources/js/Shared/FiturBaru.vue
@@ -1,15 +1,12 @@
 <template>
-    <div class="rounded-lg p-4 md:p-6 m-3 shadow-xl shadow-slate-600 bg-white">
+    <div class="rounded-lg p-4 md:p-6 m-3 shadow-xl shadow-slate-600 bg-green-100 border-4 border-green-600">
         <h3 class="text-center font-bold text-xl">Fitur Baru</h3>
-        <div class="flex flex-wrap font-serif mt-2">
-            <p>
-                <span class="font-semibold">Daftar Calon DPD Terpilih</span> diakses via pencarian dapil, halaman Detail Surat Suara, dan halaman realcount/hitung suara <span class="font-semibold">DPD</span>.
+        <div class="flex flex-wrap font-serif mt-2 text-center">
+            <p class="mb-2">
+                Fitur <span class="font-semibold text-green-900">Real Count</span> dan <span class="font-semibold text-green-900">Calon Terpilih</span> sudah selesai dikembangkan. Temukan di semua fitur pencarian maupun halaman detail surat suara. Selamat mencoba.
             </p>
             <p>
-                <span class="font-semibold">Daftar Calon DPR/DPRD Terpilih</span> segera hadir.
-            </p>
-            <p>
-                <span class="font-semibold">Real Count Pilpres </span> bisa diakses via tombol <span class="font-semibold">Real Count Pilpres</span> di Halaman Utama dan di Surat Suara Pilpres. Sedangkan <span class="font-semibold">Real Count DPD/DPR/DPRD</span> diakses melalui pencarian wilayah, dapil, halaman Detail Surat Suara, dan halaman profil calon DPD/DPR/DPRD.
+                Butuh Bantuan ? Jangan ragu <a :href="$setUrl('/hubungi-kami')" class="text-indigo-600 font-semibold underline underline-offset-2">Hubungi Kami</a>.
             </p>
         </div>
     </div>
