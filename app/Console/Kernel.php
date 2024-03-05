1e71cc8d970cc2f768a3630fd5306c882fb4efc9
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-03-05 14:57:21 +0700

    update kernel command schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('cache:clear-expired')->twiceDailyAt(9, 21, 15);
+        $schedule->command('cache:clear-expired')->twiceDailyAt(8, 20, 5);
     }
 
     /**
