660d0396280466c2e63b0b5913c57525cf09e6d2
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-03-08 07:20:27 +0700

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,15 @@
     @endisset
     @vite(['resources/js/app.js', 'resources/css/app.css', 'resources/css/nprogress.css'])
     @inertiaHead
+    <!-- Favicon Links -->
+    <link rel="icon" type="image/webp" sizes="192x192" href="{{ asset('assets/img/favicon_io/android-chrome-192x192.webp') }}">
+    <link rel="icon" type="image/webp" sizes="512x512" href="{{ asset('assets/img/favicon_io/android-chrome-512x512.webp') }}">
+    <link rel="apple-touch-icon" href="{{ asset('assets/img/favicon_io/apple-touch-icon.webp') }}">
+    <link rel="icon" type="image/webp" sizes="32x32" href="{{ asset('assets/img/favicon_io/favicon-32x32.webp') }}">
+    <link rel="icon" type="image/webp" sizes="16x16" href="{{ asset('assets/img/favicon_io/favicon-16x16.webp') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon_io/favicon.ico') }}" type="image/x-icon">
+    <link rel="manifest" href="{{ asset('assets/img/favicon_io/site.webmanifest') }}">
+
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3686539766588397"
      crossorigin="anonymous"></script>
      <!-- Google tag (gtag.js) -->
