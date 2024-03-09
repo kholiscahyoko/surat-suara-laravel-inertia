d47d9a930c3a1e6156ecbf0ec5516b69267ad1a1
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-03-09 18:36:36 +0700

    add sticky bottom ads

---
+++ resources/js/Components/BottomAds.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="sticky bottom-0">
+        <!-- horizontal1 -->
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-3686539766588397"
+            data-ad-slot="1875979364"
+            data-ad-format="auto"
+            data-full-width-responsive="true">
+        </ins>
+    </div>
+</template>
