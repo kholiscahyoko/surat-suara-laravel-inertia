6adf1f1a33584378985769ab55f191316a12c708
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-03-20 15:37:25 +0700

    add button calon terpilih to realcount dpd

--- resources/js/Shared/RealCountDpd.vue
+++ resources/js/Shared/RealCountDpd.vue
@@ -23,6 +23,9 @@
         <div class="w-full my-8">
             <HorizontalAds1 />
         </div>
+        <div class="flex justify-center mt-8 w-full">
+            <a :href="$setUrl(`/calon-terpilih/${dapil.jenis_dewan}/${$slugify(dapil.nama_dapil)}/${dapil.kode_dapil}`)" class="my-2 mx-20 md:mx-2 text-center text-lg lg:text-xl md:w-50 p-2 align-middle md:p-5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold text-white rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer">Calon Terpilih Disini</a>
+        </div>
         <div class="flex flex-wrap items-start justify-center my-8">
             <TableSuaraDpdPerWilayah :master="master" :data="data" :wilayah="wilayah" :calon="calon" :dapil="dapil"/>
         </div>
