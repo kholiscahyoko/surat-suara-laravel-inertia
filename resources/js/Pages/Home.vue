6188e7b5a8f7d79eb5e6b94801ce64ffd97d468b
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-07-13 22:19:51 +0700

    add donations to home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,7 +2,7 @@
     <h1 class="text-4xl font-bold tracking-tight text-gray-900 mb-4">
         Temukan Siapa Calon Pilihanmu
     </h1>
-    <div class="text-lg">Perlu Bantuan ? <a :href="$setUrl('/hubungi-kami')" class="hover:underline text-purple-500">Hubungi Kami</a></div>
+    <div class="text-lg">Perlu Bantuan ? <a :href="$setUrl('/hubungi-kami')" class="hover:underline text-indigo-600 font-semibold">Hubungi Kami</a></div>
     <div class="md:flex md:justify-evenly mb-4 items-center">
         <a :href="$setUrl('/cari')" class="block my-5 mx-20 md:mx-2 text-center text-lg lg:text-xl md:w-50 p-2 align-middle md:p-5 bg-gradient-to-r from-red-700 via-green-700 to-blue-700 font-semibold text-white rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer">Cari Semua Disini</a>
         <a :href="$setUrl('/hitung-suara/pilpres')" class="block my-5 mx-20 md:mx-2 text-center text-lg lg:text-xl md:w-50 p-2 md:p-5 align-middle bg-gray-600 font-semibold text-white rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer">Real Count Pilpres</a>
@@ -15,12 +15,14 @@
     <div class="w-full">
         <HorizontalAds1/>
     </div>
-    <Leaflet v-if="summary && summary.data_provinsi && geo_provinsi" :data_provinsi="summary.data_provinsi" :geo_provinsi="geo_provinsi"/>
+    <!-- <Leaflet v-if="summary && summary.data_provinsi && geo_provinsi" :data_provinsi="summary.data_provinsi" :geo_provinsi="geo_provinsi"/> -->
+    <Donations />
 </template>
 <script setup>
 import StatsBlock from '../Components/StatsBlock.vue';
-import Leaflet from '../Components/Leaflet.vue';
+// import Leaflet from '../Components/Leaflet.vue';
 import HorizontalAds1 from '../Components/HorizontalAds1.vue';
+import Donations from '../Components/Donations.vue';
 
 let props = defineProps({
     summary : Object,
