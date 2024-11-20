b873223542912cca5d052c06ace77c849a30bae7
Kholis Cahyoko <kholis.cahyoko@gmail.com>
2024-11-20 21:34:11 +0700

    add share button to surat suara pilkada

--- resources/js/Pages/SuratSuaraPilkada.vue
+++ resources/js/Pages/SuratSuaraPilkada.vue
@@ -5,9 +5,11 @@
       </h1>
     </div>
     <SuratSuaraPilkada :paslons="paslons" :wilayah="wilayah"/>
+    <FloatShare/>
 </template>
 <script setup>
 import SuratSuaraPilkada from '../Shared/SuratSuaraPilkada.vue';
+import FloatShare from '../Shared/FloatShare.vue';
 
 let props = defineProps({
     paslons: Object,
